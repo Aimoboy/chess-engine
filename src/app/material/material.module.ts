92ba252198a6b14994baca30f92c5c0c64147f41
Daniel Ellebæk Nielsen <daniogdan@gmail.com>
2022-11-20 19:19:51 +0100

    Simple front page with a top bar, a "Play" button and an "About" button

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 const materialComponents = [
-  MatToolbarModule
+  MatToolbarModule,
+  MatButtonModule
 ];
 
 
