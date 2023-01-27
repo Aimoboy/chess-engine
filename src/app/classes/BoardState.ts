2f468948ae94be108551873f1767a50e45fc28be
Daniel Ellebæk Nielsen <daniogdan@gmail.com>
2023-01-27 20:44:46 +0100

    Added possible moves to board state in front-end

--- src/app/classes/BoardState.ts
+++ src/app/classes/BoardState.ts
@@ -1,14 +1,17 @@
 import { ChessColor } from "../enums/ChessColor";
 import { WinState } from "../enums/WinState";
 import { ChessPiece } from "./ChessPiece";
+import { PossibleMove } from "./Position";
 
 export class BoardState {
     public board: (ChessPiece | null)[][];
+    public possibleMoves: PossibleMove[][][];
     public turn: ChessColor;
     public winState: WinState;
 
-    constructor(board: (ChessPiece | null)[][], turn: ChessColor, winState: WinState) {
+    constructor(board: (ChessPiece | null)[][], possibleMoves: PossibleMove[][][], turn: ChessColor, winState: WinState) {
         this.board = board;
+        this.possibleMoves = possibleMoves;
         this.turn = turn;
         this.winState = winState;
     }
